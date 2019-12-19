9cdcaf296b9a40328e40374fb5040b952ce05530
VeeVee <ville-veikko.nieminen@tuni.fi>
2019-12-19 14:08:29 +0200

    asks permission for calendar

--- memo_page/MemoScreen.js
+++ memo_page/MemoScreen.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Calendar from 'expo-calendar';
 
-export default class MemoScreene extends React.Component {
+export default function MemoScreen() {
 
-  constructor(props) {
-    super(props);
-  }
+  useEffect(() => {
+    Calendar.requestPermissionsAsync()
+      .then(res => {
+        console.log(res)
+        if(res.granted) {
+          Calendar.getCalendarsAsync()
+            .then(res => console.log(res, 'calendars'));
+        }
+      })
+    // (async () => {
+    //   const { status } = await Calendar.requestPermissionsAsync();
+    //   if (status === 'granted') {
+    //     const calendars = await Calendar.getCalendarsAsync();
+    //     console.log({ calendars });
+    //   }
+    // })();
+  },[])
 
-  static navigationOptions = {
-    title: 'Memos',
-  };
-
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <Text>Memos coming soon</Text>
-    );
-  }
-
+  return (
+    <Text>Memos coming soon</Text>
+  );
 }
